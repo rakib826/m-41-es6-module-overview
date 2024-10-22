219aff97a7f6a5fc28565699c246945ea79852ec
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-22 19:26:39 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,3 @@ function App() {
 }
 
 export default App
-echo "# m-41-es6-module-overview" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rakib826/m-41-es6-module-overview.git
-git push -u origin main
