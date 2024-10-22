fdc6951d95dc8a36e7984c871455564af38739ab
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-22 19:26:07 +0600

    es6 overview

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
+echo "# m-41-es6-module-overview" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rakib826/m-41-es6-module-overview.git
+git push -u origin main
